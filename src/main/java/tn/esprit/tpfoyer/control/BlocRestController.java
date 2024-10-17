9996724a6d08c3694ccb32543e4ec4c699914408
KhaledBouden <khaledbouden15@gmail.com>
2024-10-17 20:33:33 +0100

    git webhook test 7

--- src/main/java/tn/esprit/tpfoyer/control/BlocRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/BlocRestController.java
@@ -18,7 +18,7 @@ public class BlocRestController {
 
     IBlocService blocService;
 
-    //git webhook test 6
+    //git webhook test 7
     //http://localhost:8089/tpfoyer/bloc/retrieve-all-blocs
 
     @GetMapping("/retrieve-all-blocs")
